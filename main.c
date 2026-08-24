159cddf52dbfd7d9d2f501e2aceff800cd2af9bc
Letciasiqueira <leticiasiqueira1605@gmail.com>
2026-08-24 20:44:53 -0300

    Finalizando Jobs, rodando testes e ajeitando bugs

--- main.c
+++ main.c
@@ -18,6 +18,13 @@ typedef struct Tarefas{
     int estado_do_output;
 }Tarefas;
 
+typedef struct Job{
+    int id_do_job;
+    pid_t pid;
+    char nome_da_tarefa[100];
+    int job_finalizado; 
+}Job;
+
 void cadastro_de_tarefa(char *argumentos[], int contador_de_argumentos, Tarefas cadastradas[], int *quantidade_de_tarefas){
     
     if (contador_de_argumentos < 3){
@@ -68,14 +75,15 @@ void funcao_run_normal(char *argumentos[], int contador_de_argumentos, Tarefas c
     }
 
     if(indice == -1){
-        printf("Erro: Tarefas nao existe.\n");
+        printf("essa tarefas nao existe\n");
         return;
     }
 
-    char *argv_exec[50]}; 
+    char *argv_exec[50]; 
     argv_exec[0] = cadastradas[indice].programa;
-    
-    for(int i = 0; i < cadastradas[indice].quantidade_de_argumentos; i++){
+
+    int i;
+    for(i = 0; i < cadastradas[indice].quantidade_de_argumentos; i++){
         argv_exec[i + 1] = cadastradas[indice].argumento[i];
     }
     argv_exec[i + 1] = NULL; 
@@ -83,39 +91,39 @@ void funcao_run_normal(char *argumentos[], int contador_de_argumentos, Tarefas c
     pid_t pid = fork();
 
     if(pid < 0){
-        printf("Falha ao criar o processo.\n");
+        printf("Falha ao criar o processo\n");
         return;
 
     } else if(pid == 0){
         if(cadastradas[indice].input_do_arquivo[0] != '\0'){
         
-        int fd_in = open(cadastradas[indice].input_do_arquivo, O_RDONLY);
+        int input_do_arquivo = open(cadastradas[indice].input_do_arquivo, O_RDONLY);
         
-        if(fd_in < 0){
+        if(input_do_arquivo < 0){
             printf("nao daa pra abrir\n");
             exit(1);
         }
-        dup2(fd_in, 0);
-        close(fd_in);
+        dup2(input_do_arquivo, 0);
+        close(input_do_arquivo);
         }
 
         if(cadastradas[indice].estado_do_output == 1){
-            int fd_out = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if(fd_out < 0){
+            int output_do_arquivo = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if(output_do_arquivo < 0){
                 printf("nao da pra abrir\n");
                 exit(1);
             }
-            dup2(fd_out, 1);
-            close(fd_out);
+            dup2(output_do_arquivo, 1);
+            close(output_do_arquivo);
 
         } else if(cadastradas[indice].estado_do_output == 2){
-            int fd_out = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_APPEND, 0644);
-            if(fd_out < 0){
+            int output_do_arquivo = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_APPEND, 0644);
+            if(output_do_arquivo < 0){
                 printf("nao da para abrir\n");
                 exit(1);
             }
-            dup2(fd_out, 1);
-            close(fd_out);
+            dup2(output_do_arquivo, 1);
+            close(output_do_arquivo);
         }
         execvp(cadastradas[indice].programa, argv_exec);
 
@@ -179,33 +187,33 @@ void funcao_parallel(char *argumentos[], int contador_de_argumentos, Tarefas cad
         } else if(pid == 0){
             if(cadastradas[indice].input_do_arquivo[0] != '\0'){
         
-            int fd_in = open(cadastradas[indice].input_do_arquivo, O_RDONLY);
+            int input_do_arquivo = open(cadastradas[indice].input_do_arquivo, O_RDONLY);
         
-            if(fd_in < 0){
+            if(input_do_arquivo < 0){
                 printf("nao da para abrir\n");
                 exit(1);
             }
-            dup2(fd_in, 0);
-            close(fd_in);
+            dup2(input_do_arquivo, 0);
+            close(input_do_arquivo);
             }
 
             if(cadastradas[indice].estado_do_output == 1){
-                int fd_out = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if(fd_out < 0){
+                int output_do_arquivo = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if(output_do_arquivo < 0){
                     printf("nao da pra abrir\n");
                     exit(1);
                 }
-                dup2(fd_out, 1);
-                close(fd_out);
+                dup2(output_do_arquivo, 1);
+                close(output_do_arquivo);
 
             } else if(cadastradas[indice].estado_do_output == 2){
-                int fd_out = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_APPEND, 0644);
-                if(fd_out < 0){
+                int output_do_arquivo = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_APPEND, 0644);
+                if(output_do_arquivo < 0){
                     printf("nao da pra abrir\n");
                     exit(1);
                 }
-                dup2(fd_out, 1);
-                close(fd_out);
+                dup2(output_do_arquivo, 1);
+                close(output_do_arquivo);
             }
             execvp(cadastradas[indice].programa, argv_exec);
 
@@ -233,10 +241,10 @@ void funcao_pipe(char *argumentos[], int contador_de_argumentos, Tarefas cadastr
     int tarefas_no_total = contador_de_argumentos - 2; 
     int quantidade_de_pipe = tarefas_no_total - 1;
 
-    int pipe[10][2]; 
+    int pipes_inseridos[20][2]; 
 
     for(int a = 0; a < quantidade_de_pipe; a++){
-        if(pipe(pipe[a]) == -1){
+        if(pipe(pipes_inseridos[a]) == -1){
             printf("nao deu para criar pipe.\n");
             return;
         }
@@ -265,7 +273,8 @@ void funcao_pipe(char *argumentos[], int contador_de_argumentos, Tarefas cadastr
         char *argv_exec[50];
         argv_exec[0] = cadastradas[indice].programa;
         
-        for(int j = 0; j < cadastradas[indice].quantidade_de_argumentos; j++){
+        int j;
+        for(j = 0; j < cadastradas[indice].quantidade_de_argumentos; j++){
             argv_exec[j + 1] = cadastradas[indice].argumento[j];
         }
         argv_exec[j + 1] = NULL;
@@ -279,16 +288,16 @@ void funcao_pipe(char *argumentos[], int contador_de_argumentos, Tarefas cadastr
         } else if(pid == 0){
 
             if(ordem_da_tarefa > 0){
-                dup2(pipe[ordem_da_tarefa - 1][0], 0);
+                dup2(pipes_inseridos[ordem_da_tarefa - 1][0], 0);
             }
 
             if(ordem_da_tarefa < quantidade_de_pipe){
-                dup2(pipe[ordem_da_tarefa][1], 1);
+                dup2(pipes_inseridos[ordem_da_tarefa][1], 1);
             }
 
             for(int a = 0; a < quantidade_de_pipe; a++){
-                close(pipe[a][0]);
-                close(pipe[a][1]);
+                close(pipes_inseridos[a][0]);
+                close(pipes_inseridos[a][1]);
             }
 
             execvp(cadastradas[indice].programa, argv_exec);
@@ -303,8 +312,8 @@ void funcao_pipe(char *argumentos[], int contador_de_argumentos, Tarefas cadastr
     }
 
     for(int a = 0; a < quantidade_de_pipe; a++){
-        close(pipe[a][0]);
-        close(pipe[a][1]);
+        close(pipes_inseridos[a][0]);
+        close(pipes_inseridos[a][1]);
     }
 
     for(int i = 0; i < total_de_processos; i++){
@@ -399,7 +408,152 @@ void funcao_workdir(char *argumentos[], int contador_de_argumentos){
     }
 }
 
-int ler_linha(char *linha, Tarefas cadastradas[], int *quantidade_de_tarefas){
+void funcao_start(char *argumentos[], int contador_de_argumentos, Tarefas cadastradas[], int quantidade_de_tarefas, Job lista_de_job[], int *job_totais){
+
+    if(contador_de_argumentos < 2){
+        printf("Poucos argumentos\n");
+        return;
+    }
+
+    int indice = -1;
+    for(int i = 0; i < quantidade_de_tarefas; i++){
+        if(strcmp(cadastradas[i].nome, argumentos[1]) == 0){
+            indice = i;
+            break;
+        }
+    }
+
+    if(indice == -1){
+        printf("essa tarefa nao existe.\n");
+        return;
+    }
+
+    char *argv_exec[50];
+    argv_exec[0] = cadastradas[indice].programa;
+    int i;
+    for(i = 0; i < cadastradas[indice].quantidade_de_argumentos; i++){
+        argv_exec[i + 1] = cadastradas[indice].argumento[i];
+    }
+    argv_exec[i + 1] = NULL;
+
+    pid_t pid = fork();
+
+    if(pid < 0){
+        printf("Processo falhou ao criar.\n");
+        return;
+
+    } else if(pid == 0){
+        if(cadastradas[indice].input_do_arquivo[0] != '\0'){
+            int input_do_arquivo = open(cadastradas[indice].input_do_arquivo, O_RDONLY);
+            if(input_do_arquivo < 0){
+                printf("nao deu para abrir\n");
+                exit(1);
+            }
+            dup2(input_do_arquivo, 0);
+            close(input_do_arquivo);
+        }
+
+        if(cadastradas[indice].estado_do_output == 1){
+            int output_do_arquivo = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if(output_do_arquivo < 0){
+                printf("nao deu para abrir\n");
+                exit(1);
+            }
+            dup2(output_do_arquivo, 1);
+            close(output_do_arquivo);
+
+        } else if(cadastradas[indice].estado_do_output == 2){
+            int output_do_arquivo = open(cadastradas[indice].output_doarquivo, O_WRONLY | O_CREAT | O_APPEND, 0644);
+            if(output_do_arquivo < 0){
+                printf("nao deu para abrir\n");
+                exit(1);
+            }
+            dup2(output_do_arquivo, 1);
+            close(output_do_arquivo);
+        }
+
+        execvp(cadastradas[indice].programa, argv_exec);
+
+        printf("nao foi possivel executar.\n");
+        exit(1);
+
+    } else {
+
+        int id_novo = *job_totais + 1;
+
+        lista_de_job[*job_totais].id_do_job = id_novo;
+        lista_de_job[*job_totais].pid = pid;
+        strncpy(lista_de_job[*job_totais].nome_da_tarefa, argumentos[1], 99);
+        lista_de_job[*job_totais].nome_da_tarefa[99] = '\0';
+        lista_de_job[*job_totais].job_finalizado = 0;
+
+        (*job_totais)++;
+
+        printf("[%d] %d\n", id_novo, pid);
+    }
+}
+
+void funcao_do_job(Job lista_de_job[], int job_totais){
+
+    if(job_totais == 0){
+        printf("Nada de job por aqui.\n");
+        return;
+    }
+
+    for(int i = 0; i < job_totais; i++){
+
+        if(lista_de_job[i].job_finalizado == 0){
+            int status;
+            pid_t resultado = waitpid(lista_de_job[i].pid, &status, WNOHANG);
+
+            if(resultado == lista_de_job[i].pid){
+                lista_de_job[i].job_finalizado = 1;
+            }
+        }
+
+        if(lista_de_job[i].job_finalizado == 1){
+            printf("Job foi Finalizado\n");
+        } else {
+            printf("Job ainda está em processo\n");
+        }
+    }
+}
+
+void funcao_esperar_job(char *argumentos[], int numero_argumentos, Job lista_de_job[], int job_totais){
+
+    if(numero_argumentos < 2){
+        printf("Poucos argumentos\n");
+        return;
+    }
+
+    int procurar_id_job = atoi(argumentos[1]);
+
+    int indice = -1;
+    for(int i = 0; i < job_totais; i++){
+        if(lista_de_job[i].id_do_job == procurar_id_job){
+            indice = i;
+            break;
+        }
+    }
+
+    if(indice == -1){
+        printf("nao existe\n");
+        return;
+    }
+
+    if(lista_de_job[indice].job_finalizado == 1){
+        printf("Foi job_finalizado.\n");
+        return;
+    }
+
+    int status;
+    waitpid(lista_de_job[indice].pid, &status, 0);
+    lista_de_job[indice].job_finalizado = 1;
+
+    printf("job_finalizado\n");
+}
+
+int ler_linha(char *linha, Tarefas cadastradas[], int *quantidade_de_tarefas, Job lista_de_job[], int *job_totais){
 
     char *argumentos[MAX_argumentos];
     int contador_de_argumentos = 0;
@@ -454,6 +608,16 @@ int ler_linha(char *linha, Tarefas cadastradas[], int *quantidade_de_tarefas){
 
     } else if(strcmp(argumentos[0], "input") == 0){
         funcao_input(argumentos, contador_de_argumentos, cadastradas, *quantidade_de_tarefas);
+
+    }else if(strcmp(argumentos[0], "start") == 0){
+        funcao_start(argumentos, contador_de_argumentos, cadastradas, *quantidade_de_tarefas, lista_de_job, job_totais);
+
+    } else if(strcmp(argumentos[0], "jobs") == 0){
+        funcao_do_job(lista_de_job, *job_totais);
+
+    } else if(strcmp(argumentos[0], "wait") == 0){
+        funcao_esperar_job(argumentos, contador_de_argumentos, lista_de_job, *job_totais);
+
     }else{
         printf("Comando desconhecido\n");
     }
@@ -465,6 +629,9 @@ int main(int argc, char *argv[]){
 
     int quantidade_de_tarefas = 0;
     Tarefas cadastradas[100];
+
+    Job lista_de_job[50];
+    int job_totais = 0;
 
     if(argc == 1){
         printf("Processo interativo\n"); 
@@ -511,10 +678,10 @@ int main(int argc, char *argv[]){
                 if( contador_de_argumentos < 2){
                     printf("quantidade de argumentos invalida\n");
         
-                } else if (strcmp(argumentos[1], "funcao_sequential") == 0){
+                } else if (strcmp(argumentos[1], "sequential") == 0){
                     funcao_sequential(argumentos, contador_de_argumentos, cadastradas, quantidade_de_tarefas);
 
-                } else if (strcmp(argumentos[1], "funcao_parallel") == 0){
+                } else if (strcmp(argumentos[1], "parallel") == 0){
                     funcao_parallel(argumentos, contador_de_argumentos, cadastradas, quantidade_de_tarefas);
 
                 } else if(strcmp(argumentos[1], "pipe") == 0){
@@ -536,6 +703,15 @@ int main(int argc, char *argv[]){
 
             }else if(strcmp(argumentos[0], "append") == 0){
                 funcao_append(argumentos, contador_de_argumentos, cadastradas, quantidade_de_tarefas);
+
+            } else if(strcmp(argumentos[0], "start") == 0){
+                funcao_start(argumentos, contador_de_argumentos, cadastradas, quantidade_de_tarefas, lista_de_job, &job_totais);
+
+            } else if(strcmp(argumentos[0], "jobs") == 0){
+                funcao_do_job(lista_de_job, job_totais);
+
+            } else if(strcmp(argumentos[0], "wait") == 0){
+                funcao_esperar_job(argumentos, contador_de_argumentos, lista_de_job, job_totais);
 
             }else{
                 printf("Tarefa desconhecida\n");
@@ -562,7 +738,7 @@ int main(int argc, char *argv[]){
 
             printf("%s\n", linha); 
 
-            int acabou_a_linha = ler_linha(linha, cadastradas, &quantidade_de_tarefas);
+            int acabou_a_linha = ler_linha(linha, cadastradas, &quantidade_de_tarefas, lista_de_job, &job_totais);
 
             if(acabou_a_linha == 1){
                 break;
